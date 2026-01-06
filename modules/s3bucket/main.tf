d05c4279241cc3ab12a8d58e4d39fe4bbff5cedf
bay.gaillard <bay.gaillard@classpass.com>
2026-01-06 10:54:53 -0500

    Rename bucket

--- modules/s3bucket/main.tf
+++ modules/s3bucket/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "main" {
   provider      = aws.user
   bucket        = "classpass-atlantis-poc-${var.environment_name}"
   force_destroy = "false"           # Will prevent destruction of bucket with contents inside
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_s3_object" "objects" {
   for_each = fileset("myfiles/", "*")
-  bucket   = aws_s3_bucket.example.bucket
+  bucket   = aws_s3_bucket.main.bucket
   key      = "${each.value}"
   source   = "myfiles/${each.value}"
   etag     = filemd5("myfiles/${each.value}")
